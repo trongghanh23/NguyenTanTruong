5092ef082a8c346d246953c3210e48d096feca1e
unknown <trongghanh23>
2022-12-01 07:31:05 +0700

    bt

---
+++ module6/sprint2/shop-smartphone/src/app/component/decentralization/decentralization.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DecentralizationRoutingModule } from './decentralization-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DecentralizationRoutingModule
+  ]
+})
+export class DecentralizationModule { }
